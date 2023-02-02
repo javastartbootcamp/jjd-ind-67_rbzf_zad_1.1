edf3d5772a7efc84fd8a4f52ba59010503aa866b
rbzf <94106167+rbzf@users.noreply.github.com>
2023-02-02 17:09:26 +0100

    Add files via upload

---
+++ Zadanie1.java
@@ -0,0 +1,12 @@
+
+public class Zadanie1 {
+
+	public static void main(String[] args) {
+		System.out.println("Ucze");
+		System.out.println("sie");
+		System.out.println("programowania");
+		System.out.println("i");
+		System.out.println("commitowania");
+	}
+
+}
